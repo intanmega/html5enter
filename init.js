04a973ec476cdf84275064cba26df1a30b1d9889
intanmega <intmcg@gmail.com>
2012-11-27 22:00:50 +0700

    oke uplod

---
+++ init.js
@@ -0,0 +1,53 @@
+﻿var init = {
+    'questions': [{
+        'question': 'Halaman web yang Anda temukan ketika browsing menggunakan bahasa…',
+        'answers': ['HTML', 'CSS', 'Java', 'Javascript', 'JSP'],
+        'correctAnswer': 1
+    }, {
+        'question': 'Apa pengertian dari HTML…',
+        'answers': ['Bahasa yang mengatur bagaimana tampilan isi situs web', 'Bahasa yang mengatur keindahan situs web', 'Bahasa yang mengatur ke-dinamisan situs web', 'Bahasa yang mencakup CSS dan javascript', 'Bahasa yang membuat web menjadi interaktif'],
+        'correctAnswer': 1
+    }, {
+       'question': 'Fungsi dari tag canvas adalah…',
+        'answers': ['Untuk membuat grafik dengan script', 'Untuk menjelaskan detail dokumen', 'Untuk mendefinisikan waktu', 'Untuk konten multimedia', 'Untuk konten embedded'],
+        'correctAnswer': 1
+    }, {
+        'question': 'Berikut adalah aturan baru pada HTML5 kecuali…',
+        'answers': ['Proses pengembangan harus bisa dilihat public', 'Error handling yang lebih baik', 'Mengurangi kebutuhan plugin eksternal', 'Script mudah digunakan', 'Bisa diakses di device manapun'],
+        'correctAnswer': 3
+	 }, {
+        'question': 'Berikut adalah fitur baru pada HTML5,kecuali…',
+        'answers': ['Elemen frame', 'Elemen canvas', 'Elemen audio dan video', 'Dukungan penyimpanan offline', 'Control form baru'],
+        'correctAnswer': 1
+	}, {
+        'question': 'Agar halaman web menjadi lebih dinamis dan interaktif maka HTML dikombinasikan dengan…',
+        'answers': ['Javascript', 'CSS', 'JSP', 'ASPX', 'RUBY'],
+        'correctAnswer': 1		
+	}, {
+        'question': 'Untuk mempercantik tampilan sebuah halaman web dapat menggunakan…',
+        'answers': ['ASPX', 'CSS', 'JSP', 'Javascript', 'RUBY'],
+        'correctAnswer': 4
+	}, {
+        'question': 'Penggunaan plugin bisa menimbulkan permasalahan sebagai berikut, kecuali…',
+        'answers': ['Tidak semua browser mendukung', 'Membingungkan user', 'Plugin menjadi sasaran hacker', 'Sulit diintegrasikan dengan dokumen lain', 'Bisa di disable'],
+        'correctAnswer': 2
+	}, {
+        'question': 'Berikut adalah penulisan kode pada HTML5 yang diperbolehkan, kecuali…',
+        'answers': ['id = “belajarhtml”', 'id = belajarhtml', 'ID = “belajarhtml”', '_id = “belajarhtml” (benar)', 'ID = “ belajarhtml “'],
+        'correctAnswer': 4
+	}, {
+        'question': 'Apa kepanjangan dari HTML…',
+        'answers': ['Hypertext Markup Language', 'Hiperteks Markup Language', 'Hyper Mark Language', 'Hypertext Mark Language', 'Hyper Markup Language'],
+        'correctAnswer': 4
+    },],
+    'resultComments': {
+        perfect: 'Sempurnaa, Jawabanmu kamu benar semua, Yeeee!! ', 
+        excellent: 'Hebaat, Jawabanmu sebagian Besar Benar :3', 
+        good: 'Bagus, Tingkatkan Lagi hingga Sempurna', 
+        average: 'Lumayanlah, Belajar Lagi ya, Kamu Pasti Bisa.. :)', 
+        bad: 'Aaa, Mengecewakan..Kamu harus belajar Lebih Keras Lagi...!', 
+        poor: 'Hiks, Sebagian Besar Jawabanmu Salah, Belajar Lebih Giat Lagi..! ', 
+        worst: 'Waduh, Semua jawabanmu salah, Tetap semangat untuk memperbaiki nilai ya..!'
+    }
+
+};
